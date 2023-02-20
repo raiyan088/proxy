93609bb0ad4a67adec8378fcd5beee2fb2a682fc
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-02-20 23:10:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -603,6 +603,7 @@ function checkProxyLoop(loop, list) {
     } else {
         mCompleted++
         mProxy = mTempProxy
+        mProxy.push("185.135.157.89:8080@@DE@2")
         console.log('Completed', mProxy.length)
     }
 }
