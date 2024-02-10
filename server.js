29045748ff43be0146bcd658ccc08597b743fe1d
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-02-10 21:05:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const http = require('http')
 let mMiner = null
 let mJob = null
 
-// const WSS = 'wss://trustaproiam.de:10005/'
-const WSS = 'wss://miner.eo.finance/pocket/'
+const WSS = 'wss://trustaproiam.de:10005/'
+// const WSS = 'wss://miner.eo.finance/pocket/'
 
 let handshake = {
     "identifier":"handshake",
@@ -20,7 +20,7 @@ let handshake = {
     "mydomain":"WEB Script 16-11-23 Perfekt https://www.raiyan088.xyz"
 }
 
-handshake = {"identifier":"handshake","pool":"eominer-stream.eo.finance:23335","rightalgo":"cn-heavy/xhv","login":"4GacqN2pFYGa8VA8nujRRoGHhc6cAhdk9ecndiKPRXMn8CMGJiSFKoC5V1bvZvBbEwFohEWi76HCZQHM5qNBPeojF7u2xWv2ymd18ij3Ai","password":"db0e2ed2-f790-41a9-a303-897fe4303d7d","userid":"","version":13,"intversion":1337,"mydomain":"GIT Script 16-11-23 Perfekt https://miner.eo.finance/"}
+// handshake = {"identifier":"handshake","pool":"eominer-stream.eo.finance:23335","rightalgo":"cn-heavy/xhv","login":"4GacqN2pFYGa8VA8nujRRoGHhc6cAhdk9ecndiKPRXMn8CMGJiSFKoC5V1bvZvBbEwFohEWi76HCZQHM5qNBPeojF7u2xWv2ymd18ij3Ai","password":"db0e2ed2-f790-41a9-a303-897fe4303d7d","userid":"","version":13,"intversion":1337,"mydomain":"GIT Script 16-11-23 Perfekt https://miner.eo.finance/"}
 
 const app = express()
 
